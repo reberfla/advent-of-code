674f4c38d57836be0a303c6f5c8073c5cc1f5611
Flavio Reber <reber.flavio@gmail.com>
2024-12-01 16:49:04 +0100

    Day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftCol = mutableListOf<Int>()
+        val rightCol = mutableListOf<Int>()
+        val regex = "\\s+".toRegex()
+        input
+            .map{ regex.splitToSequence(it, 2)}
+            .forEach { leftCol.add(it.first().toInt()); rightCol.add(it.last().toInt()) }
+        leftCol.sort()
+        rightCol.sort()
+        var sum = 0
+        leftCol.zip(rightCol).forEach {
+            row -> sum += abs(row.component1() - row.component2())
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftCol = mutableListOf<Int>()
+        val rightCol = hashMapOf<Int, Int>()
+        val regex = "\\s+".toRegex()
+        input
+            .map{ regex.splitToSequence(it, 2)}
+            .forEach { leftCol.add(it.first().toInt())
+            val rightValue = it.last().toInt()
+            if (rightCol.keys.contains(rightValue)){
+                rightCol[rightValue] = rightCol[rightValue]!! + 1
+            } else {
+                rightCol[rightValue] = 1
+            }}
+        return leftCol.sumOf{if(rightCol.keys.contains(it)){ it * rightCol[it]!!} else 0}
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
